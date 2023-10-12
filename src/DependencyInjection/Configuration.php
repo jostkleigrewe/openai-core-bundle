4836a4cb03b17bafa63ed6f6c7cba33fa533e76d
Sven Jostkleigrewe <sven@jostkleigrewe.com>
2023-10-12 23:12:41 +0200

    init bundle

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jostkleigrewe\OpenAiCoreBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package Jostkleigrewe\AlexaCoreBundle\DependencyInjection
+ * @author Sven Jostkleigrewe <sven@jostkleigrewe.com>
+ * @copyright 2021 Sven Jostkleigrewe
+ */
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('jostkleigrewe_openai_core');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->scalarNode('apikey')->defaultValue('default_foo')->end()
+                ->scalarNode('organization')->defaultValue('default_bar')->end()
+            ->end();
+        return $treeBuilder;
+    }
+}
